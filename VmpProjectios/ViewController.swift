4af66dac31c583063455d9c2fd5b0839ff6a848d
metinyldz <>
2020-03-27 19:04:20 +0300

    Initial Commit

---
+++ VmpProjectios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VmpProjectios
+//
+//  Created by Metin Yıldız on 27.03.2020.
+//  Copyright © 2020 Metin Yıldız. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
