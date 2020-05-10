c781cce896cb83f7936b869e65929eed7df57371
metinyldz <>
2020-05-10 05:45:06 +0300

    Updated API Key

--- VmpProjectios/Backend/YoutubeManager.swift
+++ VmpProjectios/Backend/YoutubeManager.swift
@@ -11,7 +11,7 @@ import ObjectMapper
 import PromiseKit
 
 
-let youtube_access_token = "AIzaSyBQmoRsMPI8t5XibRv-suraDfYnfr0hYZE"
+let youtube_access_token = "AIzaSyDis0J35ZeE1GmAyo-MXNlm_t5u7yMRj7w"
 
 class YoutubeManager {
   
